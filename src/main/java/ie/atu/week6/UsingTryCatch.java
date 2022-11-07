de70b4691014c754fa17b12446ff014ea3448b72
G00402296 <G00402296@atu.ie>
2022-11-07 09:28:29 +0000

    Added Main

---
+++ src/main/java/ie/atu/week6/UsingTryCatch.java
@@ -0,0 +1,21 @@
+package ie.atu.week6;
+
+import java.util.Scanner;
+
+    public class UsingTryCatch {
+        public static void main(String[] args) {
+            System.out.println("Please Enter a Number Between 0 - 9");
+            Scanner myscan = new Scanner(System.in);
+            int myNum = myscan.nextInt();
+
+            if(myNum <= 9 || myNum > 0)
+            {
+                System.out.println("You Entered " + myNum);
+            }
+            else
+            {
+                System.out.println("Not a Valid Number");
+            }
+        }
+    }
+
